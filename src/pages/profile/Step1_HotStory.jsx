df50e7f560a098c076ad7589b1103f48b7f95be1
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-06-19 17:01:28 +0700

    add step2-hotStory

--- src/pages/profile/Step1_HotStory.jsx
+++ src/pages/profile/Step1_HotStory.jsx
@@ -2,27 +2,57 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import '@/i18n';
+import { motion } from 'framer-motion';
 
 import exit from '@/assets/images/exit.png';
+
+import Step2_HotStory from './Step2_HotStory';
 
 import './Step1_HotStory.scss';
 
 const Step1_HotStory = ({ onCancel }) => {
   const { t } = useTranslation('hotStory');
   const [name, setName] = useState('');
+  const [showListStory, setShowListStory] = useState(false);
+
+  const handleCancelFormListStory = () => {
+    setShowListStory(false);
+  };
+
+  const handleClickNext = () => {
+    //logic
+    setShowListStory(true);
+  };
 
   return (
-    <div className='step1_HotStory'>
-      <div className='div-p'>
-        <p>{t('highlight')}</p>
-        <img src={exit} onClick={onCancel} />
+    <div className='body-hotStory'>
+      <div className='step1_HotStory'>
+        <div className='div-p'>
+          <p>{t('highlight')}</p>
+          <img src={exit} onClick={onCancel} />
+        </div>
+        <div className='div-input'>
+          <input value={name} type='text' placeholder={t('highlightName')} onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div className='div-btn'>
+          <p className={name !== '' ? 'p1' : 'p2'} onClick={handleClickNext}>{t('next')}</p>
+        </div>
       </div>
-      <div className='div-input'>
-        <input value={name} type='text' placeholder={t('highlightName')} onChange={(e)=> setName(e.target.value)}/>
-      </div>
-      <div className='div-btn'>
-        <p className={name !== '' ? 'p1' : 'p2'}>{t('next')}</p>
-      </div>
+      {
+        showListStory && (
+          <div className='overlay' onClick={handleCancelFormListStory}>
+            <motion.div
+              className='list-Story-container'
+              onClick={(e) => e.stopPropagation()}
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0.5 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Step2_HotStory onCancel={handleCancelFormListStory} />
+            </motion.div>
+          </div>
+        )
+      }
     </div>
   )
 }
