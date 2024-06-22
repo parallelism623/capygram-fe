13186bb7d80e255f2d20d44140e1695de5b65d3d
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-06-22 21:49:05 +0700

    🐛 responsive LayoutMenu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -13,11 +13,78 @@ const Menu = () => {
     const location = useLocation();
 
     return (
-        <div className="menu">
-            <div className="menu-container">
-                <div className="menu-container-image">
-                    <img src={logoCapyGram} onClick={() => navigate("/")} alt="" />
+        <>
+            <div className="menu">
+                <div className="menu-container">
+                    <div className="menu-container-content">
+                        <ul>
+                            <li>
+                                <div className={`list ${location.pathname === '/' ? 'active' : ''}`} onClick={() => navigate("/")}>
+                                    <i className="fa-solid fa-house"></i>
+                                    <p>{t('text1')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="list none">
+                                    <i className="fa-solid fa-magnifying-glass"></i>
+                                    <p>{t('text2')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="list">
+                                    <i className="fa-regular fa-compass"></i>
+                                    <p>{t('text3')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className={`list ${location.pathname === '/reel' ? 'active' : ''}`} onClick={() => navigate("/reel")}>
+                                    <i className="fa-solid fa-film"></i>
+                                    <p>{t('text4')}</p>
+                                </div>
+
+                            </li>
+                            <li>
+                                <div className={`list ${location.pathname === '/messages' ? 'active' : ''}`} onClick={() => navigate("/messages")}>
+                                    <i className="fa-brands fa-facebook-messenger"></i>
+                                    <p>{t('text5')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className={`list none ${location.pathname === '/notifications' ? 'active' : ''}`} onClick={() => navigate("/notifications")}>
+                                    <i className="fa-regular fa-heart"></i>
+                                    <p>{t('text6')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="list">
+                                    <i className="fa-regular fa-square-plus"></i>
+                                    <p>{t('text7')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className={`list ${location.pathname === '/profile' ? 'active' : ''}`} onClick={() => navigate("/profile")}>
+                                    <i className="fa-solid fa-user"></i>
+                                    <p>{t('text8')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className={`list none ${location.pathname === '/threads' ? 'active' : ''}`} onClick={() => navigate("/threads")}>
+                                    <i className="fa-brands fa-threads"></i>
+                                    <p>{t('text9')}</p>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="list none">
+                                    <i className="fa-solid fa-bars"></i>
+                                    <p>{t('text10')}</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+            </div>
+
+            <div className='menu2'>
                 <div className="menu-container-content">
                     <ul>
                         <li>
@@ -26,12 +93,7 @@ const Menu = () => {
                                 <p>{t('text1')}</p>
                             </div>
                         </li>
-                        <li>
-                            <div className="list">
-                                <i className="fa-solid fa-magnifying-glass"></i>
-                                <p>{t('text2')}</p>
-                            </div>
-                        </li>
+                        
                         <li>
                             <div className="list">
                                 <i className="fa-regular fa-compass"></i>
@@ -52,12 +114,6 @@ const Menu = () => {
                             </div>
                         </li>
                         <li>
-                            <div className={`list ${location.pathname === '/notifications' ? 'active' : ''}`} onClick={() => navigate("/notifications")}>
-                                <i className="fa-regular fa-heart"></i>
-                                <p>{t('text6')}</p>
-                            </div>
-                        </li>
-                        <li>
                             <div className="list">
                                 <i className="fa-regular fa-square-plus"></i>
                                 <p>{t('text7')}</p>
@@ -69,22 +125,10 @@ const Menu = () => {
                                 <p>{t('text8')}</p>
                             </div>
                         </li>
-                        <li>
-                            <div className={`list ${location.pathname === '/threads' ? 'active' : ''}`} onClick={() => navigate("/threads")}>
-                                <i className="fa-brands fa-threads"></i>
-                                <p>{t('text9')}</p>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="list">
-                                <i className="fa-solid fa-bars"></i>
-                                <p>{t('text10')}</p>
-                            </div>
-                        </li>
                     </ul>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 export default Menu
