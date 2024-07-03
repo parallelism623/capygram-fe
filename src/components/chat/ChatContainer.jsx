39ea69395bed3dbaaecb47b2b03aa8aa6707a913
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-03 21:09:17 +0700

    add contacts-messages component

---
+++ src/components/chat/ChatContainer.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import React from 'react'
+
+import './ChatContainer.scss';
+
+const ChatContainer = () => {
+  return (
+    <div className='body-chatContainer'>
+      <div className='chat-container'>
+
+      </div>
+    </div>
+  )
+}
+
+export default ChatContainer
