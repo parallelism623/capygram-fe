fc18d23600a464c0a16f1db1715a55ebf7d28d59
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-07 21:29:23 +0700

    🐛 fix style input messages

--- src/components/chat/ChatInput.jsx
+++ src/components/chat/ChatInput.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable */
 import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next';
+import '@/i18n';
 
 import icon from '@/assets/images/icon.png';
 import microphone from '@/assets/images/microphone.png';
@@ -8,12 +10,17 @@ import heart from '@/assets/images/heart.png';
 import sendMessage from '@/assets/images/sendMessage.png';
 
 import './ChatInput.scss';
+
 const ChatInput = () => {
   const [input, setInput] = useState('');
+  const { t } = useTranslation('messages');
+  
   return (
     <div className='chatinput-container'>
       <form>
-        <input className='input-chat' type='text' value={input} />
+        <div className='input-chat'>
+          <input type='text' placeholder={t('message')} value={input} onChange={(e) => setInput(e.target.value)} />
+        </div>
 
         <img src={icon} alt='icon' className='icon-haha'/>
 
