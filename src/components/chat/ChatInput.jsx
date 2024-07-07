8acc65d3d6abb1d6d6307b132d63be503120caa4
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-07 22:56:06 +0700

    🐛 fix emoji

--- src/components/chat/ChatInput.jsx
+++ src/components/chat/ChatInput.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import '@/i18n';
+import EmojiPicker from 'emoji-picker-react';
 
 import icon from '@/assets/images/icon.png';
 import microphone from '@/assets/images/microphone.png';
@@ -14,15 +15,29 @@ import './ChatInput.scss';
 const ChatInput = () => {
   const [input, setInput] = useState('');
   const { t } = useTranslation('messages');
-  
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
+  const inputRef = React.createRef();
+
+  const addEmoji = (event, emojiObject) => {
+    const emoji = event.emoji;
+    const { selectionStart, selectionEnd } = inputRef.current;//lay vi tri con tro
+    const start = input.substring(0, selectionStart);//phan dau cua chuoi truoc con tro
+    const end = input.substring(selectionEnd, input.length);//phan cuoi cua chuoi sau con tro
+    const updateInput = start + emoji + end;
+    setInput(updateInput);
+    inputRef.current.focus();
+  };
+
   return (
     <div className='chatinput-container'>
       <form>
         <div className='input-chat'>
-          <input type='text' placeholder={t('message')} value={input} onChange={(e) => setInput(e.target.value)} />
+          <textarea type='text' placeholder={t('message')} value={input} onChange={(e) => setInput(e.target.value)} ref={inputRef} />
         </div>
 
-        <img src={icon} alt='icon' className='icon-haha'/>
+        <img src={icon} alt='icon' className='icon-haha' onClick={() => setShowEmojiPicker(!showEmojiPicker)} />
 
         <div className='action'>
           {
@@ -41,7 +56,14 @@ const ChatInput = () => {
               )
           }
         </div>
+
+        {
+          showEmojiPicker && (
+            <EmojiPicker onEmojiClick={addEmoji} className='emoj' />
+          )
+        }
       </form>
+
     </div>
   )
 }
