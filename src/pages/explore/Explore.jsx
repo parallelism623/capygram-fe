d2f04b643feabe8bb0aae4f0157b2842626236f0
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-12 23:31:09 +0700

    ✨ add Infinite Scroll

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,22 +1,44 @@
 /* eslint-disable */
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import explores from "./fakeExplore.json";
 
 import './Explore.scss';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Explore = () => {
-  const exploreData = explores.explores;
+  const [exploreData, setExploreData] = useState([]);
+  const [page, setPage] = useState(1);
 
+  useEffect(() => {
+    //api
+    const loadData = () => {
+      const exploreData = explores.explores;
+      setExploreData(prevData => [...prevData, ...exploreData]);
+    };
+
+    loadData();
+  }, [page]);
+
+
+  const fetchMoreData = () => {
+    setPage(prevPage => prevPage + 1);
+  };
 
   return (
+    <InfiniteScroll
+      dataLength={exploreData.length}
+      next={fetchMoreData}
+      hasMore={true}
+      loader={<h4>Loading...</h4>}
+    >
       <div className='body-explore'>
         {
           (exploreData && exploreData.length > 0) && exploreData.map((explore, index) => (
             <div key={index} className='item'>
               {
                 explore.media.type === 'video' ? (
-                  <video src={explore.media.url} controls></video>
+                  <video src={explore.media.url} controls/>
                 ) : (
                   <img src={explore.media.url[0]} alt='explore' />
                 )
@@ -25,7 +47,8 @@ const Explore = () => {
           ))
         }
       </div>
-    )
+    </InfiniteScroll>
+  )
 }
 
 export default Explore;
