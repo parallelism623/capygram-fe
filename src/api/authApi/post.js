0c53d19f3f33a1deeed233f06f33825fc4fca0e7
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-07-30 23:55:06 +0700

    ✨ add editPost (chưa fix)

--- src/api/authApi/post.js
+++ src/api/authApi/post.js
@@ -53,3 +53,32 @@ export const deletePostByPostId = async (postId) => {
     throw error;
   }
 }
+
+export const updatePost = async (post, user) => {
+  try {
+    const formData = new FormData();
+    const { ImageUrls, Likes, Content, Id } = post;
+    const { UserName, UserId } = user;
+    formData.append('Id', Id);
+    ImageUrls.forEach((image) => {
+      formData.append('ImageUrls', image);
+    });
+    formData.append('Likes', Likes);
+    formData.append('Content', Content);
+    formData.append('UserName', UserName);
+    formData.append('UserId', UserId);
+
+    await requestWithToken(postInstance, {
+      data: formData,
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      method: "put",
+      url: `/api/Posts/Update/${Id}`
+    });
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
