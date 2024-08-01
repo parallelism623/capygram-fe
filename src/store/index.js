038f4db4a98dbc2a99154133296ad82970675397
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-08-01 23:39:28 +0700

    chưa fix

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,28 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import formReducer from "./formSlice";
 
-export const store = configureStore({
-  reducer: {
-    form: formReducer,
-  },
-});
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, formReducer)
+let store = configureStore({
+  reducer: {
+    form: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
+})
+let persistor = persistStore(store)
+
+
+export { store, persistor }
