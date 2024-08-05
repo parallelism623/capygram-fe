c3322b821612c050880345197e85be970bcde7af
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2024-08-05 20:22:42 +0700

    🐛 fix unfollow in explore

--- src/pages/explore/CardUser.jsx
+++ src/pages/explore/CardUser.jsx
@@ -2,22 +2,26 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import '@/i18n';
+import { useNavigate } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+import { getPostByUserId } from '@/api/authApi/post';
+import { getCountFollower, getCountFollowing } from '@/api/authApi/graph';
+import UnfollowUser from './UnfollowUser';
 
 import addFriend from '@/assets/images/addFriend.png';
 import account from '@/assets/images/account.png';
 
 import './CardUser.scss';
-import { useNavigate } from 'react-router-dom';
-import { getPostByUserId } from '@/api/authApi/post';
-import { getCountFollower, getCountFollowing } from '@/api/authApi/graph';
 
-const CardUser = ({ user, Follow }) => {
+const CardUser = ({ user, Follow, setIsRender, isRender, handleClickFollow }) => {
   const { t } = useTranslation('explore');
 
   const [isFollow, setIsFollow] = useState(Follow);
   const [post, setPost] = useState(0);
   const [follower, setFollower] = useState(0);
   const [following, setFollowing] = useState(0);
+  const [showUnfollow, setShowUnfollow] = useState(false);
 
   const navigate = useNavigate();
 
@@ -37,11 +41,20 @@ const CardUser = ({ user, Follow }) => {
     };
 
     getInfo();
-  }, []);
+  }, [isRender]);
 
   const handleChat = () => {
     localStorage.setItem('userChat', JSON.stringify(user));
     navigate('/messages');
+  };
+
+  const handleCancelShowUnfollow = () => {
+    setShowUnfollow(false);
+  };
+
+  const handleFollow = () => {
+    handleClickFollow();
+    setIsFollow(true);
   };
 
   return (
@@ -71,18 +84,31 @@ const CardUser = ({ user, Follow }) => {
       <div className='bottom-card-user'>
         {
           isFollow === false ? (
-            <button className='btn-follow' onClick={() => setIsFollow(true)}>
+            <button className='btn-follow' onClick={handleFollow}>
               <img src={addFriend} alt='follow' className='icon-follow' />
               {t('follow')}
             </button>
           ) : (
             <div className='isfollow'>
               <button className='btn-message' onClick={handleChat}>{t('message')}</button>
-              <button className='btn-following'>{t('following')}</button>
+              <button className='btn-following' onClick={() => setShowUnfollow(true)}>{t('following')}</button>
             </div>
           )
         }
       </div>
+      {showUnfollow && (
+        <div className='overlay' onClick={handleCancelShowUnfollow}>
+          <motion.div
+            className='item-explore-container'
+            onClick={(e) => e.stopPropagation()}
+            animate={{ opacity: 1, scale: 1 }}
+            initial={{ opacity: 0, scale: 0.5 }}
+            transition={{ duration: 0.3 }}
+          >
+            <UnfollowUser onCancel={handleCancelShowUnfollow} user={user} setIsRender={setIsRender} isRender={isRender} setIsFollow={setIsFollow} />
+          </motion.div>
+        </div>
+      )}
     </div>
   )
 }
